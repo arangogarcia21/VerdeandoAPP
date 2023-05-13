ff5c0db24b921883f47ca13282fdbf3e74518493
arangogarcia21 <alejandroagarcia19@gmail.com>
2023-05-13 15:40:29 -0500

    first commit

---
+++ app/src/main/java/com/example/appreciclar/Activity_registro.java
@@ -0,0 +1,129 @@
+package com.example.appreciclar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class Activity_registro extends AppCompatActivity  {
+    EditText ingresarCorreo, ingresar_Contraseña, ingresarNombre, ingresarApellidos, edad, nCelular, direccion;
+
+    Button btnRegistrarse, iniciarSesion;
+    FirebaseAuth mAuth;
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+
+        ingresarCorreo = findViewById(R.id.ingresarCorreo);
+        ingresar_Contraseña = findViewById(R.id.ingresarContra);
+        ingresarNombre = findViewById(R.id.ingresarNombre);
+        ingresarApellidos = findViewById(R.id.ingresarApellidos);
+        edad = findViewById(R.id.edad);
+        nCelular = findViewById(R.id.nCelular);
+        direccion = findViewById(R.id.direccion);
+        btnRegistrarse = findViewById(R.id.btnRegistrarse);
+        iniciarSesion = findViewById(R.id.iniciarSesion);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Activity_registro.this, Login.class));
+            }
+        });
+
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String correo = ingresarCorreo.getText().toString();
+                String pass = ingresar_Contraseña.getText().toString();
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+                    ingresarCorreo.setError("Correo no válido");
+                    ingresarCorreo.setFocusable(true);
+                } else if (pass.length()<6) {
+                    ingresar_Contraseña.setError("La contraseña debe tener más de 6 letras");
+                    ingresar_Contraseña.setFocusable(true);
+                } else {
+                    REGISTRAR(correo, pass);
+                }
+            }
+        });
+
+
+
+    }
+
+    /*Método para registrar un usuario*/
+    private void REGISTRAR(String correo, String pass) {
+        mAuth.createUserWithEmailAndPassword(correo, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    assert user != null;
+                    String uid = user.getUid();
+                    String correo = ingresarCorreo.getText().toString();
+                    String contraseña = ingresar_Contraseña.getText().toString();
+                    String nombres = ingresarNombre.getText().toString();
+                    String apellidos = ingresarApellidos.getText().toString();
+                    String Edad = edad.getText().toString();
+                    String celular = nCelular.getText().toString();
+                    String Direccion = direccion.getText().toString();
+
+                    /*Mandar datos al Database*/
+                    HashMap<Object,String> DatosUsuario = new HashMap<>();
+
+                    DatosUsuario.put("uid", uid);
+                    DatosUsuario.put("correo", correo);
+                    DatosUsuario.put("contraseña", contraseña);
+                    DatosUsuario.put("nombre", nombres);
+                    DatosUsuario.put("apellido", apellidos);
+                    DatosUsuario.put("edad", Edad);
+                    DatosUsuario.put("celular", celular);
+                    DatosUsuario.put("direccion", Direccion);
+                    DatosUsuario.put("imagen", "");
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference reference = database.getReference("Usuarios");
+                    reference.child(uid).setValue(DatosUsuario);
+                    Toast.makeText(Activity_registro.this, "Se registró exitosamente", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(Activity_registro.this, Perfil.class));
+                } else {
+                    Toast.makeText(Activity_registro.this, "Algo ha salido mal", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Activity_registro.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
+
--- app/src/main/java/com/example/appreciclar/Login.java
+++ app/src/main/java/com/example/appreciclar/Login.java
@@ -1,96 +1,117 @@
 package com.example.appreciclar;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.GoogleSignatureVerifier;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Login extends AppCompatActivity {
-    Button btnRegistrar,btnLogin;
-    EditText ingresarEmail,ingresarContraseña;
-
-    FirebaseAuth firebaseAuth;
-
+    Button btnRegistrar, btnLogin;
+    EditText ingresarEmail, ingresarContraseña;
+    private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
-    
-    
+    Dialog dialog;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_login);
-        ingresarEmail = (EditText)findViewById(R.id.ingresarEmail);
-        ingresarContraseña = (EditText)findViewById(R.id.ingresarContraseña);
-        btnLogin=findViewById(R.id.Login);
-        btnRegistrar=(Button)findViewById(R.id.btnRegistrar);
+        ingresarEmail = (EditText) findViewById(R.id.ingresarEmail);
+        ingresarContraseña = (EditText) findViewById(R.id.ingresarContra);
+        btnLogin = findViewById(R.id.Login);
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
         firebaseAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
+        dialog = new Dialog(Login.this);
+
+
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               startActivity(new Intent(Login.this,activity_registro.class));
+                startActivity(new Intent(Login.this, Activity_registro.class));
+
             }
         });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                verificarCredenciales();
+                String correo = ingresarEmail.getText().toString();
+                String pass = ingresarContraseña.getText().toString();
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+                    ingresarEmail.setError("Correo inválido");
+                    ingresarEmail.setFocusable(true);
+                } else if (pass.length()<6) {
+                    ingresarContraseña.setError("Contraseña inválida");
+                    ingresarContraseña.setFocusable(true);
+                } else {
+                    LOGINUSUARIO(correo,pass);
+                }
+            }
+        });
+
+
+    }
+
+
+    //MÉTODO PARA INICIAR SESIÓN
+    private void LOGINUSUARIO(String correo, String pass) {
+
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(correo, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    progressDialog.dismiss();
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    startActivity(new Intent(Login.this,Perfil.class));
+                    Toast.makeText(Login.this, "Bienvenido a Verdeando " +user.getEmail(), Toast.LENGTH_SHORT).show();
+                    finish();
+                }else {
+                    progressDialog.dismiss();
+                    Toast.makeText(Login.this,"Algo ha salido mal", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
-    private void verificarCredenciales() {
 
-        String email = ingresarEmail.getText().toString();
-        final String pass = ingresarContraseña.getText().toString();
-
-        if (email.isEmpty() || !email.contains("@")){
-            Toast.makeText(this, "Correo invalido", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (pass.isEmpty() || pass.length()<6) {
-            Toast.makeText(this, "Contraseña invalida", Toast.LENGTH_SHORT).show();
-            return;
-        } else {
-            firebaseAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
-                         Intent intent = new Intent(Login.this,activity_registro.class);
-                         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Rellena los campos", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                }
-            });
-        }
-        progressDialog.setMessage("INGRESANDO A LA APLICACION");
-        progressDialog.show();
-         firebaseAuth.signInWithEmailAndPassword(email, pass);{
-            Toast.makeText(this, "VERIFICANDO CREDENCIALES", Toast.LENGTH_SHORT).show();
-        }
-
-
-        }
-
-    }
-
-
+}

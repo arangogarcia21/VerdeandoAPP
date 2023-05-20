ad229820cab288543f4c1c8256ada710e5021a2c
Lifebed <juliangaviri1216@gmail.com>
2023-05-20 15:37:04 -0500

    crearPost

---
+++ app/src/main/java/com/example/appreciclar/CrearPost.java
@@ -0,0 +1,14 @@
+package com.example.appreciclar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_post);
+    }
+}
